88aac5f1ec880367802b865ea87c43b4a60707da
jhwill7 <43076150+jhwill7@users.noreply.github.com>
2018-10-24 07:58:05 -0400

    Add files via upload

---
+++ Blackjack.py
@@ -0,0 +1,36 @@
+'''
+Goal: Make a text-based game of blackjack using Python
+'''
+
+'''
+Needs:
+	deck of cards
+	player's pot
+'''
+
+
+#1: Create a deck of 52 cards
+
+#create a card class
+
+#2: Shuffle the deck
+
+#3: Ask the player for their bet
+
+#4: Check player's bet against their pot
+
+#5: Deal two cards to the dealer and two to the player
+
+#6: Show only one of the dealer's cards
+
+#7: Show both of the player's cards
+
+#8: Ask the player to hit or stay
+
+#9: if the player does not bust, would they like to hit again?
+
+#10: Once the player stands, play the dealer's hand. Dealer hits until they beat the player or bust
+
+#11: Determine the winner and adjust the player's pot accordingly
+
+#12: ask the player if they'd like to play again
